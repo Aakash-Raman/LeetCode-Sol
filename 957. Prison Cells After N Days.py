c12a1f9f576d6ec826acf21740edf12aadc946f9
Aakash-Raman <54403044+Aakash-Raman@users.noreply.github.com>
2020-07-07 19:45:30 +0530

    Create 957. Prison Cells After N Days.py

---
+++ 957. Prison Cells After N Days.py
@@ -0,0 +1,19 @@
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
+        cur, first_day, count = [0]*8, None, 0
+        while N:
+            for c in range(1,7):
+                if cells[c-1] == cells[c+1]:
+                    cur[c] = 1
+                else:
+                    cur[c] = 0
+                    
+            if count == 0: #copy first day 
+                first_day = cur[:]
+            elif cur == first_day: 
+                N %= count
+            
+            cells = cur[:]
+            count += 1
+            N -= 1
+        return cur
